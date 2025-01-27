fd803f43e6343117c2b3cae51d83a86444e4d0d9
AlAminii <alaminbdt50@gmail.com>
2025-01-27 16:32:33 +0600

    error element page updated

--- src/Components/Erroelement/Erroelements.jsx
+++ src/Components/Erroelement/Erroelements.jsx
@@ -1,9 +1,13 @@
-
+import { NavLink } from "react-router-dom";
 
 const Erroelements = () => {
     return (
-        <div>
-            <h1>Opps!</h1>
+        <div className="mt-20 flex flex-col items-center text-center gap-y-4">
+            <h1 className="text-red-500 font-bold text-3xl mb-4">Ooops! Error 404</h1>
+            <p>Page is not found</p>
+            <NavLink to="/" className="bg-red-400 text-white px-4 py-3 rounded-md">
+                Go Back Home
+            </NavLink>
         </div>
     );
 };
